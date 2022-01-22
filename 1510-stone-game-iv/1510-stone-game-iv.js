6add4f4e8fe958faba4bb244436165650abc5a80
Ramanathan <47329924+imramu00@users.noreply.github.com>
2022-01-22 20:35:28 +0530

    Time: 191 ms (15.38%), Space: 41.8 MB (100.00%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+// DP problem
+var winnerSquareGame = function(n) {
+
+//             //////////////////////////////////
+//     1. Define an array that Alice will win or loss (T/F)
+//             //////////////////////////////////
+
+    const res = new Array(n+1).fill(false);
+    let k = 1;
+    
+    for (let i = 1; i <= n; i++) {
+
+//             //////////////////////////////////
+//     2. if it is square number, obviously, Alice win
+//             //////////////////////////////////
+        
+        if (k*k === i) {
+            res[i] = true;
+            k++;
+            continue;
+        }
+
+//             //////////////////////////////////
+//     3. after Alice removes j*j (1 <= j < k) stones, find whether res[i-j*j] is T/F (= Bob win/loss)
+//        if j exists that res[i-j*j] = false, it means that Bob will lose the game with the remaining i-j*j stones => Alice wins
+//        otherwise, Alice lose 
+//             //////////////////////////////////
+
+        let AliceWin = false;
+        for (let j = 1; j < k && j*j <= i; j++) {
+            if (!res[i-j*j]) {
+                AliceWin = true;
+                break;
+            }
+        }
+        res[i] = AliceWin;
+    }
+    
+    return res[n];
+};
