d8befe7f75da052df09782a8b0ca1ab264aeefe0
Ramanathan <47329924+imramu00@users.noreply.github.com>
2022-06-14 12:00:18 +0530

    Time: 117 ms (26.61%), Space: 43.4 MB (73.16%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(s) {
+  let total = 0
+  for (let i = 0; i < s.length; i += 1) {
+    total = total * 26 + s.charCodeAt(i) - "A".charCodeAt(0) + 1
+  }
+  return total
+};
